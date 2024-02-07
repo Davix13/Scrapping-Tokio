a04b99261ea42f2f6304a98f78df1a6197e6dbaf
DavidRC <david_abalo@hotmail.es>
2024-02-07 11:35:25 +0100

    El bloc tiene la forma de escrapear los videos que non funciona y el login tiene el escrapeo de tokio sin que funcione algunos videos

--- bloc.py
+++ bloc.py
@@ -1,2 +1,28 @@
-from pyhtml2pdf import converter
-converter.convert("https://www.intranet.stac.es/","prueba.pdf")
+import requests
+
+
+url = 'https://player.vimeo.com/video/875981475'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+    #'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'es-ES,es;q=0.9',
+    'Referer': 'https://learning.tokioschool.com/',  # Cambia esto a la URL de la página desde la cual proviene la solicitud
+    'Accept': 'video/mp4',
+    'Range': 'bytes=0-',
+}
+
+mis_cookies={"__cf_bm":"Fr5EDSKcA1uLNgbwWIZ8ZTfJVpmbeATTvfCLNqFjh_A-1707299366-1-ARW7LBPkdTMD4XtvD5eRtm4bEhovVVodSr/DcGl8PdWCDl5ruVUxvpJJuQ7/UBnSPDSpVqYtlKyXi2VKhgeFj+k="}
+
+response = requests.get(url, headers=headers,cookies=mis_cookies)
+print(response.status_code)
+
+
+# Guarda el contenido HTML en un archivo de texto
+with open('pagina.html', 'w', encoding='utf-8') as archivo:
+    archivo.write(response.text)
+
+print('Contenido HTML guardado en "pagina.html"')
+
+
+
--- login_google.py
+++ login_google.py
@@ -35,24 +35,27 @@ print(modulo)
 for element in modulo:
     driver.get(element)
     #bloque 2
-    modulos = driver.find_element(By.CSS_SELECTOR, "ul.flexsectionswithgrid-level-1")
-    # print(modulos.text)
+    try:
+        modulos = driver.find_element(By.CSS_SELECTOR, "ul.flexsectionswithgrid-level-1")
+        # print(modulos.text)
+        for mod in modulos.find_elements(By.CSS_SELECTOR, "li.main"):  # Los section son los modulos desplegables
+            if mod.get_attribute("class") == "section main":
+                mod.click()
+                time.sleep(1)
 
-    for mod in modulos.find_elements(By.CSS_SELECTOR, "li.main"):  # Los section son los modulos desplegables
-        if mod.get_attribute("class") == "section main":
-            mod.click()
-            time.sleep(1)
-
-    for b in modulos.find_elements(By.CSS_SELECTOR, "li.modtype_videotime"):
-        #print(b.get_attribute("id"))
-        #bueno
-        for a in b.find_elements(By.CSS_SELECTOR, "div.activityinstance"):
-            for c in a.find_elements(By.TAG_NAME, "a"):
-                print(c.get_attribute("href"))
-                url.append(c.get_attribute("href"))
+        for b in modulos.find_elements(By.CSS_SELECTOR, "li.modtype_videotime"):
+            #print(b.get_attribute("id"))
+            #bueno
+            for a in b.find_elements(By.CSS_SELECTOR, "div.activityinstance"):
+                for c in a.find_elements(By.TAG_NAME, "a"):
+                    print(c.get_attribute("href"))
+                    url.append(c.get_attribute("href"))
+    except:
+        print("No hay video")
+print(len(url))
 for element in url:
     driver.get(element)
-    time.sleep(2)
+    time.sleep(3)
     for a in driver.find_elements(By.TAG_NAME, "iframe"):
         if len(a.get_attribute("src")) > 40:
             video.append(a.get_attribute("src")[:40])
@@ -60,17 +63,26 @@ for element in url:
         else:
             print("espacio en blanco")
     time.sleep(1)
+print(len(video))
 
     # embedded_on is  the URL of site video is embedded on without query parameters.
 numero = 0
 while numero <= len(url):
+
     v = Vimeo(video[numero], url[numero])
-    stream = v.streams  # List of available streams of different quality
+    streams = v.streams  # List of available streams of different quality
     # >> [Stream(240p), Stream(360p), Stream(540p), Stream(720p), Stream(1080p)]
 
     # Download best stream
     ruta = str(video[numero][31:])
-    stream[-1].download(download_directory='video', filename= ruta)
+    try:
+        # Descargar el mejor stream
+        best_stream = streams[-1]
+        best_stream.download(download_directory='video', filename=ruta)
+
+    except Exception as e:
+        # Manejar la excepción de manera más específica
+        print(f"Error al descargar el video {ruta}: {e}")
     numero += 1
 
 
